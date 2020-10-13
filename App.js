62f9682e1b577e5a0633bff5e6283c127179a018
Kavya Pal Singh <65481230+singhkp8@users.noreply.github.com>
2020-10-13 20:48:32 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+
+import Scan from "./screens/scan";
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
+
+const TabNavigator = createBottomTabNavigator({
+  Scan: { screen: Scan },
+});
+
+const AppContainer = createAppContainer(TabNavigator);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
